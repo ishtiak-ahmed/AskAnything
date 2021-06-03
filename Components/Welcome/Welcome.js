638e98a16e722a911d455f330ba9751e07fedb31
Ishtiak Ahmed <iahmed1606@gmail.com>
2021-06-03 10:48:32 +0600

    login method added

--- Components/Welcome/Welcome.js
+++ Components/Welcome/Welcome.js
@@ -4,20 +4,30 @@ import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 import { userContext } from '../../App';
 
 export default function Welcome() {
-    const [name, setName, page, setPage] = useContext(userContext)
+    const [user, setUser, page, setPage] = useContext(userContext)
+    const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const handleLogin = () => {
-        setPage('home')
+        console.log(email, password)
+        fetch('https://ishtiak-blog.herokuapp.com/login', {
+            method: 'POST',
+            headers: { 'content-type': 'application/json' },
+            body: JSON.stringify({ email, password })
+        }).then(res => res.json())
+            .then(data => {
+                setUser(data)
+                console.log(data)
+            }).then(() => setPage('home'))
     }
     return (
         <View style={styles.container}>
             <Text>Welcome to</Text>
             <Text style={{ fontSize: '30px', margin: '20px' }}>AskAnything</Text>
             <Text>Enter your email :</Text>
-            <TextInput style={styles.input} placeholder='email' onChangeText={text => setName(text)}></TextInput>
+            <TextInput style={styles.input} placeholder='email' onChangeText={text => setEmail(text)}></TextInput>
             <Text>Enter your password :</Text>
             <TextInput style={styles.input} placeholder='password' type="password" onChangeText={text => setPassword(text)}></TextInput>
-            <Button onPress={() => handleLogin}>
+            <Button onPress={() => handleLogin()}>
                 <Text>Continue</Text>
                 Continue
             </Button>
